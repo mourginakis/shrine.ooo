3b680697c869929144f232ea48e9089054a83853
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2022-12-18 12:56:33 -0800

    rename index

--- shrine/main.py
+++ shrine/main.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def shrine():
-    return render_template('hello.html', name='Shrine.ooo', bokeh_resources=CDN.render())
+    return render_template('index.html', name='Shrine.ooo', bokeh_resources=CDN.render())
 
 
 
